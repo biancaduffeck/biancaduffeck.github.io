4d671c1777c791d3f326128566ba336a811d45f4
biancaduffeck <55994559+biancaduffeck@users.noreply.github.com>
2025-04-03 22:45:48 +0300

    Update game1.js

--- game1.js
+++ game1.js
@@ -44,15 +44,29 @@ function component(width, height, color, x, y, type) {
     this.angle = 0;
     this.moveAngle = 0;
     this.x = x;
-    this.y = y;    
+    this.y = y;
+    tempp=.5 * Math.PI
+    this.size=1
     this.update = function() {
         ctx = myGameArea.context;
+        //ctx.save();
+        //ctx.translate(this.x, this.y);
+        //ctx.rotate(this.angle);
+        //ctx.fillStyle = color;
+        //ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
+        //ctx.restore();
+
         ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.angle);
-        ctx.fillStyle = color;
-        ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
-        ctx.restore();    
+        ctx.beginPath();
+        distanceCenterX=this.x-mywidth/2
+        distanceCenterY=this.y-myheight/2
+        myAngle=Math.atan2(distanceCenterX,distanceCenterY)*-1
+        myAngle=myAngle+Math.PI/2
+        ctx.arc(mywidth/2, myheight/2, 100, myAngle-this.size/2, myAngle+this.size/2);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "white";
+        ctx.stroke();
+        ctx.restore();
     }
     this.newPos = function() {
         this.angle += this.moveAngle * Math.PI / 180;
@@ -77,8 +91,8 @@ function center(width, height, color, x, y, type) {
         ctx = myGameArea.context;
         ctx.save();
         ctx.beginPath();
-        ctx.fillStyle = "rgb(255 255 255 / 50%)";
-        ctx.arc(mywidth/2, myheight/2, 5, 0, 2 * Math.PI);
+        ctx.fillStyle = "rgb(255 255 255 / 100%)";
+        ctx.arc(mywidth/2, myheight/2, 2, 0, 2 * Math.PI);
         ctx.fill();
         ctx.restore();
     }
